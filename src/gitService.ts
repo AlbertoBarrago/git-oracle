d70a32f8a09158e4fe41f48bf01cdda8d4f79074
Alberto Barrago <albertobarrago@gmail.com>
2025-03-12 00:12:07 +0100

    trying to give a good style to log

--- src/gitService.ts
+++ src/gitService.ts
@@ -54,7 +54,7 @@ export class GitService {
     async getLog(): Promise<string> {
         try {
             const { stdout } = await execAsync(
-                'git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)" --all',
+                'git log --graph --pretty=format:"%h %ad | %s%d [%an]" --date=short',
                 { cwd: this.getWorkspaceRoot() }
             );
             return stdout;
