45dc483f9a2fee0b74036611de12d51e8c554ffa
Barrago, Alberto <abarrago@gmail.com>
2025-03-12 17:12:33 +0100

    Added toggle function

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import { GitService } from './services/gitService';
 import { BranchViewProvider, CherryPickViewProvider, LogViewProvider } from './providers/_index';
 
+
 export function activate(context: vscode.ExtensionContext) {
     const gitService = new GitService();
 
@@ -10,24 +11,26 @@ export function activate(context: vscode.ExtensionContext) {
     const branchViewProvider = new BranchViewProvider(context.extensionUri, gitService);
     const cherryPickViewProvider = new CherryPickViewProvider(context.extensionUri, gitService);
 
+    let disposables = vscode.commands.registerCommand('git-oracle.toggle', async () => {
+        // Use a simpler approach - just toggle the sidebar visibility
+        await vscode.commands.executeCommand('workbench.action.toggleSidebarVisibility');
+        
+        // After toggling, if we're opening the sidebar, make sure Git Oracle is shown
+        const config = vscode.workspace.getConfiguration('workbench');
+        const sidebarVisible = config.get('sideBar.visible', false);
+        
+        if (sidebarVisible) {
+            // If sidebar is now visible, make sure Git Oracle is shown
+            await vscode.commands.executeCommand('workbench.view.extension.git-oracle');
+        }
+    });
+
     // Register webview providers
     context.subscriptions.push(
         vscode.window.registerWebviewViewProvider('gitOracleLog', logViewProvider),
         vscode.window.registerWebviewViewProvider('gitOracleBranches', branchViewProvider),
-        vscode.window.registerWebviewViewProvider('gitOracleCherryPick', cherryPickViewProvider)
-    );
-
-    // Register commands
-    context.subscriptions.push(
-        vscode.commands.registerCommand('git-oracle.showLog', () => {
-            vscode.commands.executeCommand('workbench.view.extension.git-oracle');
-        }),
-        vscode.commands.registerCommand('git-oracle.showBranches', () => {
-            vscode.commands.executeCommand('workbench.view.extension.git-oracle');
-        }),
-        vscode.commands.registerCommand('git-oracle.cherryPick', () => {
-            vscode.commands.executeCommand('workbench.view.extension.git-oracle');
-        })
+        vscode.window.registerWebviewViewProvider('gitOracleCherryPick', cherryPickViewProvider),
+        disposables
     );
 }
 
